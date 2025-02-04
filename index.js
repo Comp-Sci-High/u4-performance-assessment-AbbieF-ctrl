d57f3c717c3e62b9fa185fddb5d1e94a1b29241c
AbbieF-ctrl <abigail.foggarthy25@compscihigh.org>
2025-02-04 19:32:34 +0000

    Did the first steps

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
+const express = require('express')
+const app = express()
 
+const cookies = [
+    {
+        name: "Chocolate Chip Cookie",
+        origin: "United States",
+        flavors: ["Classic", "Dark Chocolate", "Double Chocolate"],
+        ingredients: ["Flour", "Butter", "Sugar", "Chocolate Chips", "Eggs", "Vanilla Extract"]
+    },
+    {
+        name: "Macaron",
+        origin: "France",
+        flavors: ["Raspberry", "Pistachio", "Vanilla", "Chocolate"],
+        ingredients: ["Almond Flour", "Egg Whites", "Powdered Sugar", "Granulated Sugar", "Food Coloring"]
+    },
+    {
+        name: "Snickerdoodle",
+        origin: "Germany",
+        flavors: ["Classic", "Cinnamon Sugar", "Brown Butter"],
+        ingredients: ["Flour", "Butter", "Sugar", "Cinnamon", "Cream of Tartar", "Eggs"]
+    },
+    {
+        name: "Shortbread Cookie",
+        origin: "Scotland",
+        flavors: ["Butter", "Vanilla", "Lemon", "Chocolate Drizzle"],
+        ingredients: ["Flour", "Butter", "Sugar", "Salt"]
+    },
+    {
+        name: "Gingerbread Cookie",
+        origin: "Germany",
+        flavors: ["Classic Ginger", "Molasses", "Spiced Orange"],
+        ingredients: ["Flour", "Molasses", "Brown Sugar", "Ginger", "Cinnamon", "Nutmeg", "Eggs"]
+    }
+];
+
+app.set("view engine", "ejs")
+
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path)
+    next()
+  })
+
+  app.use(express.static(__dirname + "/public"))
+
+  app.get("/", (request, response) =>{
+   
+  })
+
+app.get("/overview", (request, response) =>{
+
+})
