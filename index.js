b0fe467d1caa38b7c59695f9e377187c9c66f360
AbbieF-ctrl <abigail.foggarthy25@compscihigh.org>
2025-02-06 19:07:19 +0000

    Mostly finished!

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
 const express = require('express')
 const app = express()
 
-const cookies = [
+const cookies = {
+    cookies: [
     {
         name: "Chocolate Chip Cookie",
+        image: "https://media.istockphoto.com/id/1455802788/photo/freshly-baked-chocolate-chip-cookies.jpg?s=612x612&w=0&k=20&c=RpizvozcpNfxr5efznmNQ3goXu11bkwG5y-oMK2XtKg=",
         origin: "United States",
         flavors: ["Classic", "Dark Chocolate", "Double Chocolate"],
-        ingredients: ["Flour", "Butter", "Sugar", "Chocolate Chips", "Eggs", "Vanilla Extract"]
+        ingredients: ["Flour", "Butter", "Sugar", "Chocolate Chips", "Eggs", "Vanilla Extract"],
+        path: "/cookie/0"
     },
     {
         name: "Macaron",
+        image: "https://preppykitchen.com/wp-content/uploads/2015/12/Macaron-blog2.jpg",
         origin: "France",
         flavors: ["Raspberry", "Pistachio", "Vanilla", "Chocolate"],
-        ingredients: ["Almond Flour", "Egg Whites", "Powdered Sugar", "Granulated Sugar", "Food Coloring"]
+        ingredients: ["Almond Flour", "Egg Whites", "Powdered Sugar", "Granulated Sugar", "Food Coloring"],
+        path: "/cookie/1"
     },
     {
         name: "Snickerdoodle",
+        image: "https://www.modernhoney.com/wp-content/uploads/2018/12/The-Best-Snickerdoodle-Cookie-Recipe-9jpg.jpg",
         origin: "Germany",
         flavors: ["Classic", "Cinnamon Sugar", "Brown Butter"],
-        ingredients: ["Flour", "Butter", "Sugar", "Cinnamon", "Cream of Tartar", "Eggs"]
+        ingredients: ["Flour", "Butter", "Sugar", "Cinnamon", "Cream of Tartar", "Eggs"],
+        path: "/cookie/2"
     },
     {
         name: "Shortbread Cookie",
+        image: "https://www.simplyrecipes.com/thmb/cyLoKdJr6HPBJtx9FNw7ZNB_QUE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Shortbread-LEAD-6-176c3c39597b474cbb2683595a535b39.jpg",
         origin: "Scotland",
         flavors: ["Butter", "Vanilla", "Lemon", "Chocolate Drizzle"],
-        ingredients: ["Flour", "Butter", "Sugar", "Salt"]
+        ingredients: ["Flour", "Butter", "Sugar", "Salt"],
+        path: "/cookie/3"
     },
     {
         name: "Gingerbread Cookie",
+        image: "https://www.skinnytaste.com/wp-content/uploads/2011/12/low-fat-gingerbread-men.jpg",
         origin: "Germany",
         flavors: ["Classic Ginger", "Molasses", "Spiced Orange"],
-        ingredients: ["Flour", "Molasses", "Brown Sugar", "Ginger", "Cinnamon", "Nutmeg", "Eggs"]
+        ingredients: ["Flour", "Molasses", "Brown Sugar", "Ginger", "Cinnamon", "Nutmeg", "Eggs"],
+        path: "/cookie/4"
     }
-];
+]
+}
 
 app.set("view engine", "ejs")
 
@@ -48,7 +60,12 @@ app.use((req, res, next) => {
   })
 
 app.get("/overview", (request, response) =>{
-response.status(200).send("Please wait for me to finish!")
+response.render("overview.ejs", cookies)
+})
+
+app.get("/cookies/:index", (request, response) =>{
+    const i = request.params.index
+    response.render("cookies.ejs", cookies.cookies[i])
 })
 
 app.listen(3000, () => {
